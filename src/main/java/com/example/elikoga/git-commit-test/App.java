407c13af63f206bf76ecb3ced441a1cf52b7eb52
Eli Kogan-Wang <elikowa@gmail.com>
2020-07-20 21:44:52 +0200

    changes

---
+++ src/main/java/com/example/elikoga/git-commit-test/App.java
@@ -0,0 +1,26 @@
+package com.example.elikoga.gitcommittest;
+
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+
+        String id = "";
+
+        try {
+        id = (new AAA()).owo();
+        } catch(Exception e){}
+        System.out.println(id);
+        System.out.println("asd");
+    }
+
+}
+
